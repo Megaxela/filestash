04bb72c827e64959465e898dc5abe5bd36f86cb3
Megaxela <xelamegaxela@gmail.com>
2025-02-14 15:25:01 +0300

    feat plugins: add minio backend plugin

--- server/plugin/index.go
+++ server/plugin/index.go
@@ -37,6 +37,7 @@ import (
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_security_svg"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_starter_http"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_video_transcoder"
+	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_backend_minio"
 )
 
 func init() {
